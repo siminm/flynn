a204dc4da59b08726d74cdd3d12722969ee8e4ea
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-12-10 14:00:27 +0900

    host: In case of an Attach log write error, return it.
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- host/libvirt_lxc_backend.go
+++ host/libvirt_lxc_backend.go
@@ -818,7 +818,7 @@ func (l *LibvirtLXCBackend) Attach(req *AttachRequest) (err error) {
 			continue
 		}
 		if _, err := w.Write([]byte(data.Message)); err != nil {
-			return nil
+			return err
 		}
 	}
 
